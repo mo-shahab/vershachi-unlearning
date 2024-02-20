fd66bfeca621131391e090b7a03d51d7ec6e2dbd
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-02-20 23:39:40 +0530

    formatted using black

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vershachi-unlearning',
-    version='0.1',
+    name="vershachi-unlearning",
+    version="0.1",
     packages=find_packages(),
 )
-
