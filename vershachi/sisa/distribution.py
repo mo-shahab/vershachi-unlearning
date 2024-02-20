a0f659d4d7764a7a1576537658a485027d30cb6a
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-02-20 07:23:51 +0530

    sisa algorithm sub-module

---
+++ vershachi/sisa/distribution.py
@@ -0,0 +1,22 @@
+def sharding(shards, distribution=, container=, dataset=, label):
+    pass
+
+def requests():
+    pass
+
+
+
+# example
+'''
+
+from vershachi import sisa
+
+
+# iris2.csv
+
+dataset = "/path/to/dataset"
+model.sisa.distribution(2, true, dataset)
+
+
+
+'''
