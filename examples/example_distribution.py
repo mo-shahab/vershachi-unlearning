4ae2bebddb8ba42609ccfc7ec1c475f395045b43
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-02-20 23:40:53 +0530

    example to use distribution submodule

---
+++ examples/example_distribution.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import json
+from scipy.sparse import load_npz
+from vershachi.sisa.preprocessing import load_data
+from vershachi.sisa.distribution import split_dataset, generate_and_distribute_requests
+
+# Paths and parameters
+datasetfile = "../datasets/datasetfile"
+container = "default"
+label = "latest"
+
+# Load data
+data = load_data(datasetfile)
+
+# Split dataset
+shards = 2  # Change this to the desired number of shards
+distribution = "uniform"  # Change this to the desired distribution
+split_dataset(shards, distribution, container, datasetfile, label=label)
+
+# Generate and distribute requests
+num_requests = 1000  # Change this to the desired number of requests
+
+# path to the splitfile.npy
+partition = "./containers/default/splitfile.npy"
+
+# generate_and_distribute_requests(num_requests, distribution, container, label, datasetfile)
+generate_and_distribute_requests(
+    num_requests, distribution, container, label, partition, datasetfile
+)
