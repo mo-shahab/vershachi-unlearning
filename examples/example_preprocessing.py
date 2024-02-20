bf274cfa54928df7717db3c8e1d6827ab2aaf146
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-02-20 20:27:15 +0530

    example script to check for the preprocessing

---
+++ examples/example_preprocessing.py
@@ -0,0 +1,8 @@
+from vershachi.sisa.preprocessing import load_data, apply_kmeans, split_data, save_split_data
+
+# Example usage
+file_paths = ['../datasets/data1.npz', '../datasets/data2.npz']
+data = load_data(file_paths)
+labels = apply_kmeans(data, num_clusters=5)  # Adjust num_clusters as needed
+X_train, X_test, y_train, y_test = split_data(data, labels)
+save_split_data(X_train, X_test, y_train, y_test)
