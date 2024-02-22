99002bd45d65b5e32a2d04567572357dbf467c52
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-02-22 20:50:24 +0530

    example file on how to use sisa module

---
+++ examples/example_sisa.py
@@ -0,0 +1,31 @@
+import json
+from sisa import SISA
+
+# Define parameters
+model = "purchase"
+train = True
+test = False
+epochs = 20
+batch_size = 16
+learning_rate = 0.001
+optimizer = "sgd"
+output_type = "argmax"
+container = "your_container_name"
+shard = 1
+slices = 1
+dataset = "path_to_your_dataset_file"
+label = "latest"
+
+# Create SISA object
+sisa = SISA(model, dataset, container, shard, slices, label=label, train=train, test=test,
+            epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
+            optimizer=optimizer, output_type=output_type)
+
+# Train model
+if train:
+    sisa.train()
+
+# Test model
+if test:
+    sisa.test()
+
