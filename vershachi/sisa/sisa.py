bd9b42879e74264f30d1008fb683232167536b0b
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-02-24 18:47:04 +0530

    fix: parameters handling in the constructor

--- vershachi/sisa/sisa.py
+++ vershachi/sisa/sisa.py
@@ -12,9 +12,28 @@ import json
 import sys  
 
 class SisaTrainer:
-    def __init__(self, args, model_dir):
-        self.args = args
+    def __init__(self, model_dir, dataset_file, model_name="purchase", train=True, test=False,
+                 epochs=20, batch_size=16, dropout_rate=0.4, learning_rate=0.001, optimizer="sgd",
+                 output_type="argmax", container="default", shard=2, slices=1, chkpt_interval=1,
+                 label="latest"):
         self.model_dir = model_dir
+        self.dataset_file = dataset_file
+        self.model_name = model_name
+        self.train = train
+        self.test = test
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.dropout_rate = dropout_rate
+        self.learning_rate = learning_rate
+        self.optimizer = optimizer
+        self.output_type = output_type
+        self.container = container
+        self.shard = shard
+        self.slices = slices
+        self.chkpt_interval = chkpt_interval
+        self.label = label
+        
+        self.input_shape, self.nb_classes = self._get_dataset_metadata()
         self.model_lib = self._import_model_module()
         self.loss_fn = CrossEntropyLoss()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -22,38 +41,47 @@ class SisaTrainer:
         self.optimizer = self._init_optimizer()
 
     def _import_model_module(self):
-        sys.path.append(self.model_dir)  # Add the model directory to the module search path
-        model_module_name = self.args.model
-        model_lib = import_module(model_module_name)
+        sys.path.append(self.model_dir)
+        model_lib = import_module(f"{self.model_name}")
         return model_lib
 
+    def _get_dataset_metadata(self):
+        # Construct the absolute path to the dataset file
+        # dataset_file_path = os.path.join(self.dataset_dir, self.dataset_file)
+        with open(self.dataset_file) as f:
+            dataset_metadata = json.load(f)
+        input_shape = tuple(dataset_metadata["input_shape"])
+        nb_classes = dataset_metadata["nb_classes"]
+        return input_shape, nb_classes
+
     def _init_model(self):
-        model = self.model_lib.Model(input_shape, nb_classes, dropout_rate=self.args.dropout_rate)
+        # Pass input_shape and nb_classes to _init_model()
+        model = self.model_lib.Model(self.input_shape, self.nb_classes, dropout_rate=self.dropout_rate)
         model.to(self.device)
         return model
 
     def _init_optimizer(self):
-        if self.args.optimizer == "adam":
-            optimizer = Adam(model.parameters(), lr=self.args.learning_rate)
-        elif self.args.optimizer == "sgd":
-            optimizer = SGD(model.parameters(), lr=self.args.learning_rate)
+        if self.optimizer == "adam":
+            optimizer = Adam(self.model.parameters(), lr=self.learning_rate)
+        elif self.optimizer == "sgd":
+            optimizer = SGD(self.model.parameters(), lr=self.learning_rate)
         else:
             raise ValueError("Unsupported optimizer")
         return optimizer
 
-    def train(self):
-        if self.args.train:
-            shard_size = sizeOfShard(self.args.container, self.args.shard)
-            slice_size = shard_size // self.args.slices
+    def _train(self):
+        if self.train:
+            shard_size = sizeOfShard(self.container, self.shard)
+            slice_size = shard_size // self.slices
             avg_epochs_per_slice = (
-                2 * self.args.slices / (self.args.slices + 1) * self.args.epochs / self.args.slices
+                2 * self.slices / (self.slices + 1) * self.epochs / self.slices
             )
             loaded = False
 
-            for sl in range(self.args.slices):
+            for sl in range(self.slices):
                 # Get slice hash using sharded lib.
                 slice_hash = getShardHash(
-                    self.args.container, self.args.label, self.args.shard, until=(sl + 1) * slice_size
+                    self.container, self.label, self.shard, until=(sl + 1) * slice_size
                 )
 
                 # Initialize state.
@@ -67,11 +95,11 @@ class SisaTrainer:
                 if not loaded:
                     # Look for a recovery checkpoint for the slice.
                     recovery_list = glob(
-                        f"containers/{self.args.container}/cache/{slice_hash}_*.pt"
+                        f"containers/{self.container}/cache/{slice_hash}_*.pt"
                     )
                     if len(recovery_list) > 0:
                         print(
-                            f"Recovery mode for shard {self.args.shard} on slice {sl}"
+                            f"Recovery mode for shard {self.shard} on slice {sl}"
                         )
 
                         # Load weights.
@@ -82,7 +110,7 @@ class SisaTrainer:
 
                         # Load time
                         with open(
-                            f"containers/{self.args.container}/times/{slice_hash}_{start_epoch}.time",
+                            f"containers/{self.container}/times/{slice_hash}_{start_epoch}.time",
                             "r",
                         ) as f:
                             elapsed_time = float(f.read())
@@ -90,13 +118,13 @@ class SisaTrainer:
                     # If there is no recovery checkpoint and this slice is not the first, load previous slice.
                     elif sl > 0:
                         previous_slice_hash = getShardHash(
-                            self.args.container, self.args.label, self.args.shard, until=sl * slice_size
+                            self.container, self.label, self.shard, until=sl * slice_size
                         )
 
                         # Load weights.
                         self.model.load_state_dict(
                             torch.load(
-                                f"containers/{self.args.container}/cache/{previous_slice_hash}.pt"
+                                f"containers/{self.container}/cache/{previous_slice_hash}.pt"
                             )
                         )
 
@@ -110,12 +138,12 @@ class SisaTrainer:
                     epoch_start_time = time()
 
                     for images, labels in fetchShardBatch(
-                        self.args.container,
-                        self.args.label,
-                        self.args.shard,
-                        self.args.batch_size,
-                        self.args.dataset,
-                        until=(sl + 1) * slice_size if sl < self.args.slices - 1 else None,
+                        self.container,
+                        self.label,
+                        self.shard,
+                        self.batch_size,
+                        self.dataset_file,
+                        until=(sl + 1) * slice_size if sl < self.slices - 1 else None,
                     ):
 
                         # Convert data to torch format and send to selected device.
@@ -142,10 +170,10 @@ class SisaTrainer:
                     # Save weights and time for every epoch.
                     torch.save(
                         self.model.state_dict(),
-                        f"containers/{self.args.container}/cache/{slice_hash}_{epoch}.pt"
+                        f"containers/{self.container}/cache/{slice_hash}_{epoch}.pt"
                     )
                     with open(
-                        f"containers/{self.args.container}/times/{slice_hash}_{epoch}.time",
+                        f"containers/{self.container}/times/{slice_hash}_{epoch}.time",
                         "w",
                     ) as f:
                         f.write("{}\n".format(train_time + elapsed_time))
@@ -153,20 +181,19 @@ class SisaTrainer:
                     # Remove previous checkpoint.
                     if epoch > 0:
                         os.remove(
-                            f"containers/{self.args.container}/cache/{slice_hash}_{epoch - 1}.pt"
+                            f"containers/{self.container}/cache/{slice_hash}_{epoch - 1}.pt"
                         )
                         os.remove(
-                            f"containers/{self.args.container}/times/{slice_hash}_{epoch - 1}.time"
+                            f"containers/{self.container}/times/{slice_hash}_{epoch - 1}.time"
                         )
 
                 # If this is the last slice, create a final checkpoint.
-                if sl == self.args.slices - 1:
+                if sl == self.slices - 1:
                     os.rename(
-                        f"containers/{self.args.container}/cache/{slice_hash}_{slice_epochs - 1}.pt",
-                        f"containers/{self.args.container}/cache/shard-{self.args.shard}_{self.args.label}.pt"
+                        f"containers/{self.container}/cache/{slice_hash}_{slice_epochs - 1}.pt",
+                        f"containers/{self.container}/cache/shard-{self.shard}_{self.label}.pt"
                     )
                     os.rename(
-                        f"containers/{self.args.container}/times/{slice_hash}_{slice_epochs - 1}.time",
-                        f"containers/{self.args.container}/times/shard-{self.args.shard}_{self.args.label}.time"
+                        f"containers/{self.container}/times/{slice_hash}_{slice_epochs - 1}.time",
+                        f"containers/{self.container}/times/shard-{self.shard}_{self.label}.time"
                     )
-
