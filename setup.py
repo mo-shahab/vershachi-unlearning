0a6c2f8053d720b1138c180d6eec10c854b354bd
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-03-03 17:28:30 +0530

    fix: setup.py file long description error

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(
     name="vershachi-unlearning",
     version="0.1",
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
